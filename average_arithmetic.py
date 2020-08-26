871efaf303e5bb73a5f00350ffe2c19538347c23
Juliana Furtado <juliana.jsf95@gmail.com>
2020-08-25 21:00:17 -0300

    Faz a média aritmética.

---
+++ average_arithmetic.py
@@ -0,0 +1,7 @@
+print("Hi! This code does the calculation of arithmetic average between four notes.")
+x1=float(input("Enter with the first value: "))
+x2=float(input("Enter with the second value: "))
+x3=float(input("Enter with the third value: "))
+x4=float(input("Enter with the fourth value: "))
+average=(x1+x2+x3+x4)/4
+print("A média aritmética é",average)
